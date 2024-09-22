ce496eed984ae6448bd7c4fb4e525ada012e62a5
Thuc <>
2024-09-23 00:33:01 +0700

    Fix

--- react-native.config.js
+++ react-native.config.js
@@ -3,7 +3,7 @@ module.exports = {
       ios: {},
       android: {
         sourceDir: './android',
-        packageName: 'com.oponiti.tamtalk',
+        packageName: 'com.oponiti.tamtalk.biz',
       },
     },
   }
