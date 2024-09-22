5328cffa24352daf18fc54450c6e7925d2e00df5
Thuc <>
2024-09-22 23:22:56 +0700

    ok

--- android/app/src/main/java/com/detective/NativeAppPackage.kt
+++ android/app/src/main/java/com/detective/NativeAppPackage.kt
@@ -4,15 +4,33 @@ import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.uimanager.ViewManager
+import android.view.View
+import com.facebook.react.uimanager.ReactShadowNode
+//class NativeAppPackage : ReactPackage {
+//
+//    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager> {
+//        return emptyList()
+//    }
+//
+//    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
+//        val modules = mutableListOf<NativeModule>()
+//
+//        modules.add(AppNativeModule(reactContext))
+//        modules.add(BadgeModule(reactContext))
+//        return modules
+//    }
+//}
 
 class NativeAppPackage : ReactPackage {
 
-    override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager> {
-        return emptyList()
-    }
+    override fun createViewManagers(
+        reactContext: ReactApplicationContext
+    ): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
 
-    override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-        val modules = mutableListOf<NativeModule>()
+    override fun createNativeModules(
+        reactContext: ReactApplicationContext
+    ): MutableList<NativeModule> {
+                val modules = mutableListOf<NativeModule>()
 
         modules.add(AppNativeModule(reactContext))
         modules.add(BadgeModule(reactContext))
