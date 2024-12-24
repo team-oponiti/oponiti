afd7594cfd00f573511b31b6763b4cff32e92593
Full Name <email@address.com>
2024-12-24 12:32:03 +0700

    fix code

--- src/define/webviewUri.js
+++ src/define/webviewUri.js
@@ -5,8 +5,8 @@ import { NativeModules} from 'react-native';
 // export const account = NativeModules.AppNativeModule.account ||  "https://tamtalk.com/detective-account/"
 // export const more = NativeModules.AppNativeModule.more ||  "https://tamtalk.com/detective-more/"
 // export const blog = NativeModules.AppNativeModule.blog ||  "https://tamtalk.com/blog/"
-export const domain =   "ddokddok.co"
-export const login =   "https://ddokddok.co"
-export const logout =   "https://ddokddok.co"
+export const domain =   "expert.ddokddok.co"
+export const login =   "https://expert.ddokddok.co"
+export const logout =   "https://expert.ddokddok.co"
 export const environment = NativeModules.AppNativeModule.env; 
 // /http://43.203.213.15/ 
