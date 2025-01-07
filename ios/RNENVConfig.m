87c524272d0e45cf06f8cbd47be06a4ea2665575
Full Name <email@address.com>
2025-01-07 10:06:07 +0700

    fix

--- ios/RNENVConfig.m
+++ ios/RNENVConfig.m
@@ -29,14 +29,14 @@ RCT_EXPORT_MODULE();
   
   return @{
     @"env": @"prod",
-    @"chat": @"http://43.203.213.150/chat/",
-    @"membership": @"http://43.203.213.150/detective-membership/",
-    @"account": @"http://43.203.213.150/detective-account/",
-    @"more": @"http://43.203.213.150/detective-more/",
-    @"blog": @"http://43.203.213.150/blog/",
-    @"domain": @"43.203.213.150",
-    @"login": @"http://43.203.213.150/detective-login/",
-    @"logout": @"http://43.203.213.150/logout",
+    @"chat": @"http://43.203.234.150/chat/",
+    @"membership": @"http://43.203.234.150/detective-membership/",
+    @"account": @"http://43.203.234.150/detective-account/",
+    @"more": @"http://43.203.234.150/detective-more/",
+    @"blog": @"http://43.203.234.150/blog/",
+    @"domain": @"43.203.234.150",
+    @"login": @"http://43.203.234.150/detective-login/",
+    @"logout": @"http://43.203.234.150/logout",
   };
 #else
   return @{
