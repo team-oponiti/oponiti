abe6c906190373d060a205d919d0c60f762bd399
Full Name <email@address.com>
2025-09-10 11:05:36 +0700

    fix

--- src/common/messages.js
+++ src/common/messages.js
@@ -3,44 +3,45 @@
 export default {
     en_US: {
         Home: "Home", 
-        Request: "Request",
-        News: "News",
+        Search: "Search",
+        Health: "Health",
         Review: "Review",
-        Me:"Me",
+        Profile:"Profile",
         "buttonError": "Try again",
         "titleError": "Please check the internet",
-        "messageError": "Please check your internet\nOr reload Table on app again."
+        "messageError": "Please check your internet\nOr reload app again."
     }, 
     ko_KR: {
-        Community: "커뮤니티",
         Home: "홈", 
-        Request: "상담관리",
-        News: "병원소식",
-        Review: "후기관리",
-        Me:"병원정보",
+        Search: "병원찾기",
+        Health: "찜", 
+        Profile:"내정보",
+        Community:"커뮤니티",
+        "Info": "암 지식관",
         "buttonError": "다시 시도",
         "titleError": "네트워크 연결을 확인해주세요.",
         "messageError": "힐메이트는 인터넷 연결이 꼭 필요해요.\n연결상태를 확인하고 다시 시도해 주세요."
     }, 
     ko: {
-        Community: "커뮤니티",
+         "Info": "암 지식관",
         Home: "홈", 
-        Request: "상담관리",
-        News: "병원소식",
-        Review: "후기관리",
-        Me:"병원정보",
+        Search: "병원찾기",
+        Health: "찜", 
+        Profile:"내정보",
+        Community:"커뮤니티",
         "buttonError": "다시 시도",
         "titleError": "네트워크 연결을 확인해주세요.",
         "messageError": "힐메이트는 인터넷 연결이 꼭 필요해요.\n연결상태를 확인하고 다시 시도해 주세요."
     }, 
     en: {
         Home: "Home", 
-        Request: "Request",
-        News: "News",
+        Search: "Search",
+        Health: "Health",
         Review: "Review",
-        Me:"Me",
+        Profile:"Profile",
         "buttonError": "Try again",
         "titleError": "Please check the internet",
-        "messageError": "Please check your internet\nOr reload Table on app again."
+        "messageError": "Please check your internet\nOr reload app again."
     }
+    
 }
