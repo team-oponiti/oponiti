a895941803417f8d88d3641bb8a66b17299436a4
Full Name <email@address.com>
2025-09-19 14:21:16 +0700

    fix: upgrade 0.77

--- ios/detective/AppDelegate.h
+++
@@ -1,7 +0,0 @@
-#import <RCTAppDelegate.h>
-#import <UIKit/UIKit.h>
-#import <UserNotifications/UserNotifications.h>
-#import <UserNotifications/UserNotifications.h> // nếu chưa có
-
-@interface AppDelegate : RCTAppDelegate <UNUserNotificationCenterDelegate>
-@end
